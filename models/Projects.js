cddf2d44772b1507ad66dc6fc8953733e886ec3c
coderbennett <jbennett@csumb.edu>
2022-05-16 17:55:20 -0700

    add projects model

---
+++ models/Projects.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+Project.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        repository: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        deployed: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+        },
+        media: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'project'
+    }
+);
+
+module.exports = Project;
