fd8cfad6b3dcc8b213fda4ae914bfbc7845dbfbd
coderbennett <jbennett@csumb.edu>
2022-05-16 18:12:30 -0700

    add project api routes

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const projectData = await Project.findAll();
+
+        res.status(200).json(projectData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const projectData = await Project.findByPk(req.params.id);
+
+        res.status(200).json(projectData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newProject = await Project.create({ ...req.body });
+        res.json(newProject);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const projectData = Project.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (projectData) {
+            res.status(200).end();
+        } else {
+            res.status(400).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
